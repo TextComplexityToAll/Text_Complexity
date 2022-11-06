aa800be02cc2143b8670ba779e360626404f8237
KHMary <117572699+KHMary@users.noreply.github.com>
2022-11-06 14:37:02 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="TextComplexity",
+    version="0.0.1",
+    author="Mariia_Anastasia",
+    author_email="khoreva.mary@yandex.ru",
+    description="A package to evaluate text complexity",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_package/homepage/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
